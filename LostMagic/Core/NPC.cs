c7aa5dd64f8f475c4071b9fe7476d1bbce59e7e9
Gigoto <gigotoserv3@gmail.com>
2018-10-21 10:16:14 +0200

    commentaire

--- LostMagic/Core/NPC.cs
+++ LostMagic/Core/NPC.cs
@@ -11,6 +11,6 @@ namespace LostMagic.Core
         //Les NPC (non playable caracters) dérivent de la classe Entity
         //Les NPC, contrairement aux joueurs, ne possèdent pas de mana et leurs déplacements
         //sont gérés automatiquement.
-        //Les NPC ont une gamme de sorts limitée.
+        //Les NPC ont une gamme de sorts limitée voire nulle.
     }
 }
